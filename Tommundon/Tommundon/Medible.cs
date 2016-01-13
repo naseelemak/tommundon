0a4378169a53157bbc5068f8ed99b6eaf4473a01
Jaedon Tan <taseelemak@gmail.com>
2016-01-13 13:35:39 +0800

    Nurse table

--- Tommundon/Tommundon/Medible.cs
+++ Tommundon/Tommundon/Medible.cs
@@ -9,16 +9,6 @@ namespace Tommundon
 {
     public class Medible
     {
-        //nurse
-        public int nurseID { get; set; }
-        public string nurseName { get; set; }
-        public int nurseRank { get; set; }
-        //patients
-        public int patientID { get; set; }
-        public string patientName { get; set; }
-        public int dayleft { get; set; }
-        public string ward { get; set; }
-        public bool discharge { get; set; }
 
 
         public static OleDbConnection AquireConnection()
@@ -27,7 +17,7 @@ namespace Tommundon
             string executable = System.Reflection.Assembly.GetExecutingAssembly().Location;
             string path = (System.IO.Path.GetDirectoryName(executable));
             AppDomain.CurrentDomain.SetData("DataDirectory", path);
-            OleDbConnection connect = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\Medible.mdb;User Id=admin; Password=;");
+            OleDbConnection connect = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\Ward Management System 2.mdb;User Id=admin; Password=;");
             return connect;
         }
         public virtual void insert(nurse item)
--- Tommundon/Tommundon/Nurse.cs
+++ Tommundon/Tommundon/Nurse.cs
@@ -9,15 +9,17 @@ namespace Tommundon
 {
     public class nurse : Medible
     {
-        public new int nurseID { get; set; }
-        public new string nurseName { get; set; }
-        public new int nurseRank { get; set; }
+        public string NurseID { get; set; }
+        public string NurseName { get; set; }
+        public int Rank { get; set; }
+        public string WardID { get; set; }
 
-        public nurse(int _nurseID, string _nurseName, int _nurseRank) 
+        public nurse(string _NurseID, string _NurseName, int _Rank, string _WardID) 
         {
-            this.nurseID = _nurseID;
-            this.nurseName = _nurseName;
-            this.nurseRank = _nurseRank;
+            this.NurseID = _NurseID;
+            this.NurseName = _NurseName;
+            this.Rank = _Rank;
+            this.WardID = _WardID;
         }
 
         public nurse()
@@ -31,8 +33,8 @@ namespace Tommundon
             connect.Open();
             OleDbCommand cmd = new OleDbCommand();
             cmd.Connection = connect;
-            string query = "('" + item.nurseID.ToString() + "','" + item.nurseName + "','" + item.nurseRank.ToString() + "')";
-            cmd.CommandText = "INSERT into Nurselist ( nurseID, nurseName, nurseRank) values" + query;
+            string query = "('" + item.NurseID + "','" + item.NurseName + "','" + item.Rank.ToString() + "','" + item.WardID + "')";
+            cmd.CommandText = "INSERT into Nurses ( NurseID, NurseName, Rank, WardID ) values" + query;
             cmd.ExecuteNonQuery();
             connect.Close();
         }
